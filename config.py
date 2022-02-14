aad26a74f036818c077c7bacb872d75e44b7e434
ana laura sosa <99158086+ana-acosta@users.noreply.github.com>
2022-02-14 13:16:55 -0600

    Katas

---
+++ config.py
@@ -0,0 +1,25 @@
+import os
+def main():
+    try:
+        configuration = open('config.txt',"w")
+    except FileNotFoundError as err:
+        print("Couldn't find the config.txt file!")
+    except IsADirectoryError:
+        print("Found config.txt but it is a directory, couldn't read it")
+    except (BlockingIOError, TimeoutError):
+        print("Filesystem under heavy load, can't complete reading configuration file")
+
+
+def main1():
+    try:
+        open("config.txt")
+    except OSError as err:
+        if err.errno == 2:
+            print("Couldn't find the config.txt file!")
+        elif err.errno == 13:       
+             print("Found config.txt but couldn't read it")
+
+
+
+if __name__ == '__main__':
+    main1()
